2ab332f88311e7ca1667f36540532c53c1ca6a44
Surja Kumar Jana <learn.surja01@gmail.com>
2025-11-13 02:52:38 +0530

    first code done

---
+++ READ.md
@@ -0,0 +1,2 @@
+# dsa-collectionFramework
+This repository contains Data Structures and Algorithms (DSA) implementations in Java, along with practical examples and utilities using Java Collections Framework.
---
+++ src/array/ArrayBasic.java
@@ -0,0 +1,13 @@
+package array;
+
+public class ArrayBasic {
+
+    public static void main(String[] args) {
+        System.out.println("this is a sample program to start with array");
+        int[] arr = {1,2,3,4,5,6,54};
+        System.out.println(arr);
+        for(int i =0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
