02b2cf68b21de1e34cfdda2223369b869dc4099b
Surja Kumar Jana <edu.surja0110@gmail.com>
2026-05-16 19:35:34 +0530

    16/05/2026 -> LinkedList question, Circular LinkedList question

---
+++ src/linkedList/CircularLinkedListTraversal.java
@@ -0,0 +1,18 @@
+package linkedList;
+
+
+/*
+problem url: https://www.geeksforgeeks.org/problems/circular-linked-list-traversal/1
+ */
+public class CircularLinkedListTraversal {
+    /* Function to print nodes in a given Circular linked list */
+    void printList(Node head) {
+        if (head == null) return;
+        Node temp = head;
+        while(temp.next!=head){
+            System.out.print(temp.data+" ");
+            temp = temp.next;
+        }
+        System.out.print(temp.data);
+    }
+}
