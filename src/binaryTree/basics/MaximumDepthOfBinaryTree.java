5f40218c917fe6d7c77255310bfca876a3b81552
Surja Kumar Jana <edu.surja0110@gmail.com>
2026-06-18 22:25:13 +0530

    18/06/2026 -> Binary Tree questions

---
+++ src/binaryTree/basics/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,16 @@
+package binaryTree.basics;
+
+import binaryTree.TreeNode;
+
+/*
+problem url: https://leetcode.com/problems/maximum-depth-of-binary-tree/
+ */
+public class MaximumDepthOfBinaryTree {
+    public int maxDepth(TreeNode root) {
+        //base case:
+        if(root == null) return 0;
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return (leftDepth>rightDepth)? leftDepth+1: rightDepth+1;
+    }
+}
